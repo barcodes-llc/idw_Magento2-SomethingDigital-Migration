38223eac5cd2f2d13d14e8a3597735be52b23045
Gil Greenberg <ggreenberg@somethingdigital.com>
2017-05-05 10:46:36 -0400

    Preload Config Resource Model for config migrations
    For example:
    
            $this->resourceConfig->saveConfig(
                'catalog/frontend/list_mode',
                'grid',
                'default',
                0
            );

--- Model/Migration/Generator.php
+++ Model/Migration/Generator.php
@@ -25,18 +25,21 @@ use SomethingDigital\Migration\Api\MigrationInterface;
 use SomethingDigital\Migration\Helper\Cms\Page as PageHelper;
 use SomethingDigital\Migration\Helper\Cms\Block as BlockHelper;
 use SomethingDigital\Migration\Helper\Email\Template as EmailHelper;
+use Magento\Config\Model\ResourceModel\Config as ResourceConfig;
 
 class ' . $name . ' implements MigrationInterface
 {
     protected $page;
     protected $block;
     protected $email;
+    protected $resourceConfig;
 
-    public function __construct(PageHelper $page, BlockHelper $block, EmailHelper $email)
+    public function __construct(PageHelper $page, BlockHelper $block, EmailHelper $email, ResourceConfig $resourceConfig)
     {
         $this->page = $page;
         $this->block = $block;
         $this->email = $email;
+        $this->resourceConfig = $resourceConfig;
     }
 
     public function execute(SetupInterface $setup)
