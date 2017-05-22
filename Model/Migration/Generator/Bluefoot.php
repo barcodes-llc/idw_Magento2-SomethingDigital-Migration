dc5edbb4dfe239d112ed3fda873a990db915b89e
Alex Borisenko <aborisenko@somethingdigital.com>
2017-05-22 18:16:07 +0300

    Add helper for bluefoot entities management

--- Model/Migration/Generator/Bluefoot.php
+++ Model/Migration/Generator/Bluefoot.php
@@ -75,6 +75,7 @@ use Magento\Framework\Setup\SetupInterface;
 use SomethingDigital\Migration\Api\MigrationInterface;
 use SomethingDigital\Migration\Helper\Cms\Page as PageHelper;
 use SomethingDigital\Migration\Helper\Cms\Block as BlockHelper;
+use SomethingDigital\Migration\Helper\Cms\Bluefoot as BluefootHelper;
 use SomethingDigital\Migration\Helper\Email\Template as EmailHelper;
 use Magento\Config\Model\ResourceModel\Config as ResourceConfig;
 
@@ -82,13 +83,15 @@ class ' . $name . ' implements MigrationInterface
 {
     protected $page;
     protected $block;
+    protected $bluefoot;
     protected $email;
     protected $resourceConfig;
 
-    public function __construct(PageHelper $page, BlockHelper $block, EmailHelper $email, ResourceConfig $resourceConfig)
+    public function __construct(PageHelper $page, BlockHelper $block, BluefootHelper $bluefoot, EmailHelper $email, ResourceConfig $resourceConfig)
     {
         $this->page = $page;
         $this->block = $block;
+        $this->bluefoot = $bluefoot;
         $this->email = $email;
         $this->resourceConfig = $resourceConfig;
     }
