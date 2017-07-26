c1adf6117877cb667297d04311b29973d96cef3e
Todd Christensen <tchristensen@somethingdigital.com>
2017-07-26 10:27:56 -0700

    Fix UsageExceptions in Bluefoot generators.
    When failing to look up existing blocks.

--- Model/Migration/Generator/Bluefoot/Block.php
+++ Model/Migration/Generator/Bluefoot/Block.php
@@ -2,9 +2,10 @@
 
 namespace SomethingDigital\Migration\Model\Migration\Generator\Bluefoot;
 
-use SomethingDigital\Migration\Model\Migration\Generator\Bluefoot as BluefootGenerator;
-use SomethingDigital\Migration\Model\Cms\BlockRepository;
 use Magento\Framework\Api\SearchCriteriaBuilder;
+use SomethingDigital\Migration\Exception\UsageException;
+use SomethingDigital\Migration\Model\Cms\BlockRepository;
+use SomethingDigital\Migration\Model\Migration\Generator\Bluefoot as BluefootGenerator;
 use SomethingDigital\Migration\Model\Migration\Generator\Escaper;
 
 class Block implements GeneratorInterface
